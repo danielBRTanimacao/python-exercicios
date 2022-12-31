62c09805e61204527dbe19ff057c23b595d47627
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2022-12-31 09:29:20 -0300

    exercicio

---
+++ python exercicios/ex049.py
@@ -0,0 +1,9 @@
+print("=-"*12)
+print("programa tabuada")
+print("=-"*12)
+num = int(input("Digite um número para ver a tabuada de 0 a 10 do mesmo: "))
+for c in range(0, 11):
+    print(f"{num} x {c} = {num*c}")
+print("=-"*12)
+print("Fim do programa")
+print("=-"*12)
