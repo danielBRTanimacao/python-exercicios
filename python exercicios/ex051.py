effa9807483925c2439370fa60e5bf457ca5570e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-01-01 10:12:09 -0300

    outro exercicio

---
+++ python exercicios/ex051.py
@@ -0,0 +1,5 @@
+ter = int(input("Digite o primeiro termo: "))
+raz = int(input("Digite a razão: "))
+for c in range(ter, 10+1, raz):
+    print(c)
+print(f"O termo inicial e {ter}, e sua razão e {raz}")
