99bde89a39bbd0b3ed98b7da92e88593802cd3ca
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-01-02 12:40:10 -0300

    mais exercicios

---
+++ python exercicios/ex052.py
@@ -0,0 +1,8 @@
+n = int(input("Digite um número inteiro: "))
+for i in range(0, n+1):
+    print(i)
+if n % n == 1:
+    print("Esse numero e inteiro ")
+else:
+    print("não e inteiro")
+# esse programa ta errado
