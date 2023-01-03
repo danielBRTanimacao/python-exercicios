7882a05387cf4ff27d882061f62cfa1fc40ee1f8
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-01-03 10:17:54 -0300

    mais exercicios

---
+++ python exercicios/ex054.py
@@ -0,0 +1,7 @@
+for c in range(1, 8):
+    nas = int(input(f"Digite o ano de nascimento da {c}° pessoa: "))
+if 2022 - nas == 18:
+    print(f"os maiores de idade são {c}")
+else: 
+    print(f"Os menores de idade são {c}")
+
