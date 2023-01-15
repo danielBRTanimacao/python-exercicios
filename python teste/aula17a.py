126297e61af4d0d9bf6563b546fa5a97e03e9246
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-01-15 13:13:40 -0300

    atualizando aula

--- python teste/aula17a.py
+++ python teste/aula17a.py
@@ -1,3 +1,19 @@
-num = [1, 2, 4, 8, 5]
+'''num = [1, 2, 4, 8, 5]
 num[2] = 6 #LISTA
+num.append(9)
+num.insert(3, 2)
+if 20 in num:
+    num.remove(20)
+else:
+    print('Número não foi encontrado')
+num.sort(reverse=True)
+#num.pop(3) #Remove o parametro final
 print(num)
+print(f'Essa lista tem {len(num)} elementos')
+valores = []
+for cont in range(0, 5):
+    valores.append(int(input("Digite o valor: ")))
+valores.sort() #so para testar
+for c, v in enumerate(valores):
+    print(f"O valor {v}... Foi encontrado na posição {c}")
+print("Fim")'''
