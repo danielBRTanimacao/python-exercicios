b999401d58900d44cb9a7f9de91cf909117ae727
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-02-11 10:02:33 -0300

    update aula 21 python

--- python teste/aula21a.py
+++ python teste/aula21a.py
@@ -1,2 +1,33 @@
-help(input.__doc__)
+'''help(input.__doc__)
 help(input)
+def contador(i, m, f):
+    """
+    -> Faz uma contagem e mostra na tela
+    :parametro i: iniciu da minha contagem
+    :parametro m: fim da minha contagem
+    :parametro f: em quantos numeros ele ira pular
+    :return : sem retorno
+    """
+    c = i
+    while c <= m:
+        print(f"{c}...", end='')
+        c += f
+    print("FIM!")
+
+
+contador(2, 10, 2) #DOCsTRING 
+
+def somar(a, b, c=0): #Parametro opcinal
+    """
+    -> Essa fução irar somar 3 valores
+    :parametro a > ira receber um valor
+    :parametro b > ira receber um valor
+    :parametro c > ira receber um valor
+    :return: nao retorna
+    """
+    soma = a + b + c
+    print(f'A soma vale {soma}')
+
+
+somar(2, 6, 3)
+'''
