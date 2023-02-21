fc680c3f2ff3ba848c1a2a04bb71c73db0a3d9e3
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-02-21 12:25:03 -0300

    exercicio python

--- python exercicios/ex101.py
+++ python exercicios/ex101.py
@@ -1,1 +1,15 @@
-def
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f"Com {idade} anos voto NEGADO!"
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos voto opcional!'
+    else:
+        return f'Com {idade} anos Voto OBRIGATORIO'
+
+
+#programa principal
+nasc = int(input("Em que ano você nasceu? "))
+print(voto(nasc))
