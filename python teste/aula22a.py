c088f986f52a60eccd17092ea0a59f3170f9c47b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-02-28 13:10:42 -0300

    update aula 22 python

--- python teste/aula22a.py
+++ python teste/aula22a.py
@@ -1,1 +1,10 @@
-print('aa')
+def fatorial(n):
+    f = 1
+    for c in range(1, n+1):
+        f *= c
+    return f
+
+
+num = int(input("Digite um número para ver seu fatorial: "))
+fat = fatorial(num)
+print(f"O Fatorial de {num} é {fat}")
