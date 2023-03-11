05dfbb4a1503211f5e2c70cd730c4a69cf2cb3a0
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-03-11 16:52:12 -0300

    ultima aula update

--- python exercicios/ex115/lib/arquivo/__init__.py
+++ python exercicios/ex115/lib/arquivo/__init__.py
@@ -1,2 +1,30 @@
+from lib.interface import *
+
 def arquivoExiste(nome):
-    pass
+    try:
+        a = open(nome,'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+    
+
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print("Houve um erro na criação do arquivo")
+    else:
+        print(f"O arquivo {nome} foi criado com sucesso!")
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('ERRO ao ler o arquivo')
+    else:
+        cabecalho('Pessoas cadastradas')
+        print(a.read())
