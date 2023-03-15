9c70ed4317db8ce07ae310c0e128d2be23100e3d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-03-15 08:00:51 -0300

    update exercicio final

--- python exercicios/ex115/lib/arquivo/__init__.py
+++ python exercicios/ex115/lib/arquivo/__init__.py
@@ -27,7 +27,10 @@ def lerArquivo(nome):
         print('ERRO ao ler o arquivo')
     else:
         cabecalho('Pessoas cadastradas')
-        print(a.read())
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f"{dado[0]:<30}{dado[1]:>3} anos")
     finally:
         a.close()
 
